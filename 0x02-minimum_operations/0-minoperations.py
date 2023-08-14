781b15ab657c9079bf410204dba45b0249c800b6
bdot100 <bidemiokunade@gmail.com>
2023-08-14 12:05:50 +0100

    Function added

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a minOperations Function"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    Arguments: n - integer
+    """
+    no_of_operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            no_of_operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return no_of_operations
+    
