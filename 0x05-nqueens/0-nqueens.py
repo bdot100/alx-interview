f2f1308f30d22abeeaa1a7d25825aeb1d8d3e1c7
bdot100 <bidemiokunade@gmail.com>
2023-08-31 18:50:55 +0100

    Nqueens problem solved

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,10 +3,13 @@
 This program solves the N queens problem.
 
 Usage: nqueens N
-If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
+If the user called the program with the wrong number of arguments,
+print Usage: nqueens N, followed by a new line, and exit with the status 1
 where N must be an integer greater or equal to 4
-If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
-If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
+If N is not an integer, print N must be a number, followed
+by a new line, and exit with the status 1
+If N is smaller than 4, print N must be at least 4, followed
+by a new line, and exit with the status 1
 The program should print every possible solution to the problem
 One solution per line
 Format: see example
@@ -14,6 +17,7 @@ You don’t have to print the solutions in a specific order
 You are only allowed to import the sys module
 """
 import sys
+
 
 def is_safe(board, row, col):
     """Check row"""
@@ -24,16 +28,18 @@ def is_safe(board, row, col):
             return False
     return True
 
+
 def solve_nqueens(N, row=0, board=[]):
     """solves the Nqeens problem"""
     if row == N:
-        print([ [r, c] for r, c in enumerate(board) ])
+        print([[r, c] for r, c in enumerate(board)])
         return
     for col in range(N):
         if is_safe(board, row, col):
             board.append(col)
             solve_nqueens(N, row+1, board)
             board.pop()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -44,9 +50,9 @@ if __name__ == "__main__":
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     solve_nqueens(N)
