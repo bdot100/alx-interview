fbe6bb3c51875b825b5a624960467f384a79c103
bdot100 <bidemiokunade@gmail.com>
2023-09-25 12:29:53 +0100

    Making Change Algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,23 +15,55 @@ def makeChange(coins, total):
     If total cannot be met by any number of coins you have, return -1
     coins is a list of the values of the coins in your possession
     The value of a coin will always be an integer greater than 0
-    You can assume you have an infinite number of each denomination of coin in the list
+    You can assume you have an infinite number of each
+    denomination of coin in the list
     Your solution’s runtime will be evaluated in this task
     """
-    if total < 0:
-        return 0  # If total is negative, return 0.
-    
-    # Create a list to store the minimum number of coins needed for each total from 0 to 'total'.
-    # Initialize all values with a large number to start.
-    dp = [float('inf')] * (total + 1)
-    
-    # The minimum number of coins needed to make change for 0 is 0.
-    dp[0] = 0
-    
-    # Iterate through each coin denomination and update the dp list.
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    # If dp[total] is still 'inf', it means no combination of coins can make the total.
-    return dp[total] if dp[total] != float('inf') else -1
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
+
+    # if total < 0:
+    #     return 0  # If total is negative, return 0.
+
+    # # Create a list to store the minimum number of coins
+    # needed for each total from 0 to 'total'.
+    # # Initialize all values with a large number to start.
+    # dp = [float('inf')] * (total + 1)
+
+    # # The minimum number of coins needed to make change for 0 is 0.
+    # dp[0] = 0
+
+    # # Iterate through each coin denomination and update the dp list.
+    # for coin in coins:
+    #     for amount in range(coin, total + 1):
+    #         dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    # # If dp[total] is still 'inf', it means no combination
+    # of coins can make the total.
+    # return dp[total] if dp[total] != float('inf') else -1
