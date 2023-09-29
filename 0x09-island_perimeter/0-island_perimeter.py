bb7324227fe6b6d7a4503639b6b8a5a7e771c9b3
bdot100 <bidemiokunade@gmail.com>
2023-09-29 13:51:38 +0100

    Island Perimeter calculation done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This Function returns the perimeter of the
+island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    This Function returns the perimeter of the
+    island described in grid
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4  # Start with assuming all four sides are exposed
+
+                # Check adjacent cells (up, down, left, and right)
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2  # Subtract 2 for the shared edge
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for the shared edge
+
+    return perimeter
