81aa1305c75f05ddcfe6e919f6e72430f9f46edd
bdot100 <bidemiokunade@gmail.com>
2023-09-29 13:53:44 +0100

    Island Perimeter calculation done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -19,7 +19,8 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                perimeter += 4  # Start with assuming all four sides are exposed
+                # Start with assuming all four sides are exposed
+                perimeter += 4
 
                 # Check adjacent cells (up, down, left, and right)
                 if row > 0 and grid[row - 1][col] == 1:
